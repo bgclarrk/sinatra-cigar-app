880ad65254f03ae4394a325ba97ab78bb8b45edc
+ <+>
2021-02-27 14:52:45 -0700

    Add login/signup routes and mount controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
     get '/users/login' do
-
+        erb :'/users/login'
     end
 
     get '/users/signup' do
-
+        erb :'/users/signup'
     end
 
 end
